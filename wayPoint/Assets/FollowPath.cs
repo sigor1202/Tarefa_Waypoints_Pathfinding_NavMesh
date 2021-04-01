b27354d902862e3313ffa69085f5ecf8e4d3657d
sigor1202 <sigor7829@gmail.com>
2021-04-01 18:44:09 -0300

    TarefaWP

---
+++ wayPoint/Assets/FollowPath.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    //transform do objetivo
+    Transform goal;
+    //velocidade que o tanque se move
+    float speed = 5.0f;
+    //distancia entre o objetivo e o tanque para que mude o objetivo
+    float accuracy = 1.0f;
+    //velocidade de rotação
+    float rotSpeed = 2.0f;
+
+    //variavel para pegar o GameObject em que esta o escript WPManager
+    public GameObject wpManager;
+    //lista de WayPoints
+    GameObject[] wps;
+    //nó atual
+    GameObject currentNode;
+    //Waypoint atual
+    int currentWP = 0;
+    //variavel do grafo
+    Graph g;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pega os WayPoints do wpManager e atribui a  variavel wps
+        wps = wpManager.GetComponent<WpManager>().waypoints;
+        //pega o graph do wpManager e atribui a  variavel g
+        g = wpManager.GetComponent<WpManager>().graph;
+        //iguala o nó atual a posição zero dowps
+        currentNode = wps[0];
+    }
+
+    //função para ir ao heliporto
+    public void GoToHeli()
+    {
+        //calcula o caminho para o waypoit 1
+        g.AStar(currentNode, wps[1]);
+        currentWP = 0;
+    }
+
+    //função para ir ate as ruinas
+    public void GoToRuin()
+    {
+        //calcula o caminho para o waypoit 6
+        g.AStar(currentNode, wps[6]);
+        currentWP = 0;
+    }
+
+    void LateUpdate()
+    {
+        //não fazer nada se a lista de nós for igual a zero ou se p wayPoint atrual for igual ao tamanho da lista de nós
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        //o nó que estara mais proximo neste momento
+        currentNode = g.getPathPoint(currentWP);
+
+        //se estivermos proximos o bastante do nó o tanque se movera pra o proximo
+        if (Vector3.Distance(g.getPathPoint(currentWP).transform.position, transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        //se o numero do Waypoint atual for menor que a lista
+        if(currentWP<g.getPathLength())
+        {
+            //goal é igual a posição do WayPoint atual
+            goal = g.getPathPoint(currentWP).transform;
+            //posição do objetivo
+            Vector3 lookAtGoal = new Vector3(goal.position.x,
+                this.transform.position.y,
+                goal.position.z);
+            //direção entre o objetivo e o tanque
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            //faz o player rotacionar no sentido do vetor
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                Quaternion.LookRotation(direction),
+                Time.deltaTime * rotSpeed);
+                
+        }
+
+        //faz o tanque se mover
+        transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+}
---
+++ wayPoint/Assets/WpManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//faz aparecer no inspector
+[System.Serializable]
+public struct link
+{
+    //direção do tanque
+    public enum direction { UNI, BI }
+    //nó  1 e nó 2
+    public GameObject node1;
+    public GameObject node2;
+    //direção
+    public direction dir;
+}
+
+public class WpManager : MonoBehaviour
+{
+    //array de waypoints
+    public GameObject[] waypoints;
+    //array de links
+    public link[] links;
+    //graph
+    public Graph graph = new Graph();
+
+     void Start()
+    {
+        //se a lista de waypoints for maior que zero
+        if (waypoints.Length > 0)
+        {
+            //para cada GameObject wp cria um nó
+            foreach (GameObject wp in waypoints)
+            {
+                graph.AddNode(wp);
+            }
+            //para cada link i cria node de ida se for bi cria de ida e de volta
+            foreach(link i in links)
+            {
+                graph.AddEdge(i.node1, i.node2);
+                if (i.dir == link.direction.BI)
+                    graph.AddEdge(i.node2, i.node1);
+            }
+        }
+
+    }
+
+    void Update()
+    {
+        //desenhos os grafos na tela
+        graph.debugDraw();
+    }
+
+}
